a9a5c14d6e947b864ccbd11619b766631817bfa5
JAuxance <auxance.jourdan@proton.me>
2026-01-12 10:15:59 +0100

    Add print_arguments.py script to display command line arguments

---
+++ debugging/print_arguments.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#import sys
+
+#for i in range(len(sys.argv)):
+    #print(sys.argv[i])
+
+#$ ./print_arguments.py 1 2 3
+#print_arguments.py
+#1
+#2
+#3
+import sys
+
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
+# 0 error detected
